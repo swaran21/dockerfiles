50a7962221510d8adca737e35dcafa3dbe86b6d1
swaran21 <saiswaran212005@gmail.com>
2025-06-29 16:34:18 +0530

    Add debug logs to entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 set -e
 
-/opt/keycloak/bin/kc.sh start --optimized --http-port=${PORT:-8080}
+echo "🧪 entrypoint kicked off, PORT=$PORT"
+/opt/keycloak/bin/kc.sh show-config
+
+echo "✅ Starting Keycloak on port ${PORT:-8080}"
+exec /opt/keycloak/bin/kc.sh start --optimized --http-port=${PORT:-8080}
